359f9fbc33b884a013072c6f3122fefd825901eb
Haoyu Xia <55463354+HavenXia@users.noreply.github.com>
2021-05-06 21:16:32 -0400

    Create DataBase.java

---
+++ src/DataBase.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+public class DataBase implements IDatabase {
+
+    @Override
+    public void loadDatabase(String filePath) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<Movie> searchByTitle(String title) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Movie> searchByYear(int startYear, int endYear) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Movie> searchByRating(double lowerBound, double upperBound) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Movie> searchByRating(List<String> genres) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
